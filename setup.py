6d6b0e8054f13c38fcf42e16aac9c6598ebfecbf
LMBooth <liambooth123@hotmail.co.uk>
2023-05-15 23:14:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages= ['pybci', 'pybci.examples'],
+    packages= find_packages(),#['pybci', 'pybci.examples'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
