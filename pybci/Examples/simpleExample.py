4a710d7aa1e1f799b38e4e24016dcb403e79d585
LMBooth <liambooth123@hotmail.co.uk>
2023-05-16 01:10:30 +0100

    Update simpleExample.py

--- pybci/Examples/simpleExample.py
+++ pybci/Examples/simpleExample.py
@@ -1,21 +1,18 @@
-import time, sys
-#sys.path.append('../')  # add the parent directory of 'utils' to sys.path, whilst in beta build.
+import time
 from pybci import PyBCI
-
 bci = PyBCI()
 while not bci.connected:
     bci.Connect()
     time.sleep(1)
-
 bci.TrainMode()
 while(True):
     currentMarkers = bci.ReceivedMarkerCount()
     time.sleep(1) # wait for marker updates
+    print(currentMarkers)
     if len(currentMarkers) > 1:  # check there is more then one marker type received
         if min([currentMarkers[key][1] for key in currentMarkers]) > 10:
             bci.TestMode()
             break
-
 try:
     while True:
         time.sleep(1)
