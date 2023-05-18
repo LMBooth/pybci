111a7a8d512fc98213d373f212e3fc1e403a8c17
LMBooth <liambooth123@hotmail.co.uk>
2023-05-18 15:30:29 +0100

    pypi

--- build_package.py
+++ build_package.py
@@ -3,11 +3,18 @@ import subprocess
 import sys
 
 def main():
-    package_name = os.getenv('PYPI_PACKAGE_NAME', 'pybci')
-    os.environ['PYPI_PACKAGE_NAME'] = package_name
+    package_name = os.getenv('PYPI_PACKAGE_NAME', 'default-name')
+
+    with open('setup.py', 'r') as f:
+        setup_contents = f.read()
+
+    setup_contents = setup_contents.replace("'default-name'", f"'{package_name}'")
+
+    with open('temp_setup.py', 'w') as f:
+        f.write(setup_contents)
 
     # Run the build command
-    subprocess.check_call([sys.executable, '-m', 'build'])
+    subprocess.check_call([sys.executable, '-m', 'build', '--setup-py', 'temp_setup.py'])
 
 if __name__ == "__main__":
     main()
