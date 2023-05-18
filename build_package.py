96da5b59360e033b8c85cfe736db87c724a738a5
LMBooth <liambooth123@hotmail.co.uk>
2023-05-18 15:04:34 +0100

    testpypi and pypi differing package names

---
+++ build_package.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+import sys
+
+def main():
+    package_name = os.getenv('PYPI_PACKAGE_NAME', 'default-name')
+    os.environ['PYPI_PACKAGE_NAME'] = package_name
+
+    # Run the build command
+    subprocess.check_call([sys.executable, '-m', 'build'])
+
+if __name__ == "__main__":
+    main()
