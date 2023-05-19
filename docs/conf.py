695c2a5f5359d1e64630fe0866df3f643714145c
LMBooth <31129153+LMBooth@users.noreply.github.com>
2023-05-20 00:24:24 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
+import PyBCI
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
