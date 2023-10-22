8f1967680ab647bc149581013c4384dfea368177
LMBooth <liambooth123@hotmail.co.uk>
2023-10-22 23:22:00 +0100

    small cli updates

--- pybci/CliTests/testSklearn.py
+++ pybci/CliTests/testSklearn.py
@@ -3,33 +3,36 @@ import time
 import argparse
 from sklearn.neural_network import MLPClassifier
 
-def main(create_pseudo_device=True, min_epochs_train=4, min_epochs_test=10):
+def main(createPseudoDevice=True, min_epochs_train=4, min_epochs_test=10):
     clf = MLPClassifier(max_iter = 1000, solver ="lbfgs")#solver=clf, alpha=alpha,hidden_layer_sizes=hid)
     if min_epochs_test <= min_epochs_train:
         min_epochs_test = min_epochs_train+1
-    bci = PyBCI(minimumEpochsRequired = min_epochs_train, createPseudoDevice=create_pseudo_device, clf = clf)
+    bci = PyBCI(minimumEpochsRequired = min_epochs_train, createPseudoDevice=createPseudoDevice, clf = clf)
     while not bci.connected: # check to see if lsl marker and datastream are available
         bci.Connect()
         time.sleep(1)
     bci.TrainMode() # now both marker and datastreams available start training on received epochs
     accuracy = 0
+    test = False
     try:
         while(True):
-            currentMarkers = bci.ReceivedMarkerCount() # check to see how many received epochs, if markers sent to close together will be ignored till done processing
-            time.sleep(0.5) # wait for marker updates
-            print("Markers received: " + str(currentMarkers) +" Accuracy: " + str(round(accuracy,2)), end="         \r")
-            if len(currentMarkers) > 1:  # check there is more then one marker type received
-                if min([currentMarkers[key][1] for key in currentMarkers]) > bci.minimumEpochsRequired:
-                    classInfo = bci.CurrentClassifierInfo() # hangs if called too early
-                    accuracy = classInfo["accuracy"]
-                if min([currentMarkers[key][1] for key in currentMarkers]) > min_epochs_train:  
-                    bci.TestMode()
-                    break
-        while True:
-            markerGuess = bci.CurrentClassifierMarkerGuess() # when in test mode only y_pred returned
-            guess = [key for key, value in currentMarkers.items() if value[0] == markerGuess]
-            print("Current marker estimation: " + str(guess), end="           \r")
-            time.sleep(0.2)
+            if test is False:
+                currentMarkers = bci.ReceivedMarkerCount() # check to see how many received epochs, if markers sent to close together will be ignored till done processing
+                time.sleep(0.5) # wait for marker updates
+                print("Markers received: " + str(currentMarkers) +" Accuracy: " + str(round(accuracy,2)), end="         \r")
+                if len(currentMarkers) > 1:  # check there is more then one marker type received
+                    if min([currentMarkers[key][1] for key in currentMarkers]) > bci.minimumEpochsRequired:
+                        classInfo = bci.CurrentClassifierInfo() # hangs if called too early
+                        accuracy = classInfo["accuracy"]
+                    if min([currentMarkers[key][1] for key in currentMarkers]) > min_epochs_train:  
+                        bci.TestMode()
+                        break
+            else:
+                markerGuess = bci.CurrentClassifierMarkerGuess() # when in test mode only y_pred returned
+                guess = [key for key, value in currentMarkers.items() if value[0] == markerGuess]
+                print("Current marker estimation: " + str(guess), end="           \r")
+                time.sleep(0.2)
+        return None
     except KeyboardInterrupt: # allow user to break while loop
         print("\nLoop interrupted by user.")
 
@@ -38,5 +41,6 @@ if __name__ == '__main__':
     parser.add_argument("--createPseudoDevice", default=True, type=bool, help="Set to True or False to enable or disable pseudo device creation. pseudodevice generates 8 channels of 3 marker types and baseline.")
     parser.add_argument("--min_epochs_train", default=4, type=int, help='Minimum epochs to collect before model training commences, must be less than, min_epochs_test. If less than min_epochs_test defaults to min_epochs_test+1.')
     parser.add_argument("--min_epochs_test", default=14, type=int, help='Minimum epochs to collect before model testing commences, if less than min_epochs_test defaults to min_epochs_test+1.')
+
     args = parser.parse_args()
     main(**vars(args))
--- pybci/CliTests/testTensorflow.py
+++ pybci/CliTests/testTensorflow.py
@@ -3,8 +3,8 @@ import argparse
 from pybci import PyBCI
 import tensorflow as tf# bring in tf for custom model creation
 
-def main(create_pseudo_device=True, min_epochs_train=4, min_epochs_test=10, num_chs = 8, num_feats = 2, num_classes = 4):
-    if create_pseudo_device:
+def main(createPseudoDevice=True, min_epochs_train=4, min_epochs_test=10, num_chs = 8, num_feats = 2, num_classes = 4):
+    if createPseudoDevice:
         num_chs = 8 # 8 channels are created in the PseudoLSLGenerator
         num_feats = 2 # default is mean freq and rms to keep it simple
         num_classes = 4 # number of different triggers (can include baseline) sent, defines if we use softmax of binary
@@ -20,29 +20,33 @@ def main(create_pseudo_device=True, min_epochs_train=4, min_epochs_test=10, num_
     model.compile(loss='sparse_categorical_crossentropy',# using sparse_categorical as we expect multi-class (>2) output, sparse because we encode targetvalues with integers
               optimizer='adam',
               metrics=['accuracy'])
-    bci = PyBCI(minimumEpochsRequired = min_epochs_train, createPseudoDevice=create_pseudo_device, model = model)
+    bci = PyBCI(minimumEpochsRequired = min_epochs_train, createPseudoDevice=createPseudoDevice, model = model)
     while not bci.connected: # check to see if lsl marker and datastream are available
         bci.Connect()
         time.sleep(1)
     bci.TrainMode() # now both marker and datastreams available start training on received epochs
     accuracy = 0
+    test = False
     try:
         while(True):
-            currentMarkers = bci.ReceivedMarkerCount() # check to see how many received epochs, if markers sent to close together will be ignored till done processing
-            time.sleep(0.5) # wait for marker updates
-            print("Markers received: " + str(currentMarkers) +" Accuracy: " + str(round(accuracy,2)), end="         \r")
-            if len(currentMarkers) > 1:  # check there is more then one marker type received
-                if min([currentMarkers[key][1] for key in currentMarkers]) > bci.minimumEpochsRequired:
-                    classInfo = bci.CurrentClassifierInfo() # hangs if called too early
-                    accuracy = classInfo["accuracy"]
-                if min([currentMarkers[key][1] for key in currentMarkers]) > min_epochs_test:  
-                    bci.TestMode()
-                    break
-        while True:
-            markerGuess = bci.CurrentClassifierMarkerGuess() # when in test mode only y_pred returned
-            guess = [key for key, value in currentMarkers.items() if value[0] == markerGuess]
-            print("Current marker estimation: " + str(guess), end="           \r")
-            time.sleep(0.2)
+            if test is False:
+                currentMarkers = bci.ReceivedMarkerCount() # check to see how many received epochs, if markers sent to close together will be ignored till done processing
+                time.sleep(0.5) # wait for marker updates
+                print("Markers received: " + str(currentMarkers) +" Accuracy: " + str(round(accuracy,2)), end="         \r")
+                if len(currentMarkers) > 1:  # check there is more then one marker type received
+                    if min([currentMarkers[key][1] for key in currentMarkers]) > bci.minimumEpochsRequired:
+                        classInfo = bci.CurrentClassifierInfo() # hangs if called too early
+                        accuracy = classInfo["accuracy"]
+                    if min([currentMarkers[key][1] for key in currentMarkers]) > min_epochs_test:  
+                        bci.TestMode()
+                        break
+            else:
+                markerGuess = bci.CurrentClassifierMarkerGuess() # when in test mode only y_pred returned
+                guess = [key for key, value in currentMarkers.items() if value[0] == markerGuess]
+                print("Current marker estimation: " + str(guess), end="           \r")
+                time.sleep(0.2)
+
+        return None
     except KeyboardInterrupt: # allow user to break while loop
         print("\nLoop interrupted by user.")
 
@@ -53,6 +57,6 @@ if __name__ == '__main__':
     parser.add_argument("--min_epochs_test", default=14, type=int, help='Minimum epochs to collect before model testing commences, if less than min_epochs_test defaults to min_epochs_test+1.')
     parser.add_argument("--num_chs", default=8, type=int, help='Num of channels in data stream to configure tensorflow model, if PseudoDevice==True defaults to 8.')
     parser.add_argument("--num_classes", default=4, type=int, help='Num of classes in marker stream to configure tensorflow model, if PseudoDevice==True defaults to 4.')
-    
+
     args = parser.parse_args()
     main(**vars(args))
