94d3f251d4bee1c52c9c1ccc89e7f27a710bd8a0
LMBooth <liambooth123@hotmail.co.uk>
2023-10-28 13:03:17 +0100

    Update LSLScanner.py

--- pybci/Utils/LSLScanner.py
+++ pybci/Utils/LSLScanner.py
@@ -66,6 +66,8 @@ class LSLScanner:
         markerStreams = []
         self.markerStream = None
         for stream in streams:
+            print(stream.name())
+            print(stream.type())
             if stream.type() in self.markerTypes:
                 markerStreams.append(StreamInlet(stream))
         if self.markerStreamPredefined:
