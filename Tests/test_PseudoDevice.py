a53110e9c1690ac3122967bdb185a54ba6482de6
LMBooth <31129153+LMBooth@users.noreply.github.com>
2023-11-04 02:11:13 +0000

    Update test_PseudoDevice.py

--- Tests/test_PseudoDevice.py
+++ Tests/test_PseudoDevice.py
@@ -3,13 +3,13 @@ from pybci.Utils.PseudoDevice import PseudoDeviceController
 
 def test_run_pseudo():
 
-    pd = PseudoDeviceController(execution_mode="thread")
+    pd = PseudoDeviceController(is_multiprocessing=True)
     pd.BeginStreaming()
     time.sleep(5)
     pd.StopStreaming()
     time.sleep(5)
 
-    pd = PseudoDeviceController(execution_mode="process")
+    pd = PseudoDeviceController(is_multiprocessing=False)
     pd.BeginStreaming()
     time.sleep(5)
     pd.StopStreaming()
