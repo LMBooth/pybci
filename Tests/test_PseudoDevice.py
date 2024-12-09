8b189fada398932a6fe1546c60f17066ac548a31
LMBooth <liambooth123@hotmail.co.uk>
2024-12-09 19:26:19 +0000

    update to 3.12

--- Tests/test_PseudoDevice.py
+++ Tests/test_PseudoDevice.py
@@ -2,7 +2,8 @@ import time
 from pybci.Utils.PseudoDevice import PseudoDeviceController
 
 def test_run_pseudo():
-
+    expected_duration = "~40 minutes"  # Adjust this to your expected time
+    print(f"\n\n=== WARNING: The tests are expected to take {expected_duration} total! ===\n")
     pd = PseudoDeviceController(is_multiprocessing=True)
     pd.BeginStreaming()
     time.sleep(5)
